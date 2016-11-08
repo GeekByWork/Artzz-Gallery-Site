ed4c0eeab99e51856727a7311ff58569222b5355
Umang Saxena <umang.saxena@sjsu.edu>
2016-11-08 00:53:47 -0800

    Carousel Changes

--- Gallery/FullSizeView.php
+++ Gallery/FullSizeView.php
@@ -24,43 +24,24 @@ else
 
 $visitedCount = $_COOKIE[$paintingName];
 
-    echo 0;
-    if(count($_COOKIE['recentlyVisited'])==0)
-        echo "Empty COOKIE['recentlyVisited']";
-    $my_array = $_COOKIE['recentlyVisited'];
-    echo 1;
-    foreach ($my_array as $x)
-    {
-        echo $x;
-    }
-    echo 2;
-        if(array_search($paintingName, $my_array)==1)
+if(isset($_COOKIE["recentlyVisited"])) {
+    $my_array = $_COOKIE["recentlyVisited"];
+    if(array_search($paintingName, $my_array)==1)
     {
         $to_remove = array($paintingName);
         $result = array_diff($my_array, $to_remove);
     }
-
-    echo 3;
-    foreach ($result as $x)
-    {
-        echo $x;
-    }
-    echo 4;
-if(count($result)==0)
+    if(count($result)==0)
+        $result = array($paintingName);
+    else
+        array_unshift($result, $paintingName);
+}
+else
+{
     $result = array($paintingName);
-else
-    array_unshift($result, $paintingName);
+}
 
-    setcookie('recentlyVisited', $result);
-    $_COOKIE['recentlyVisited'] = $result;
-
-    foreach ($_COOKIE['recentlyVisited'] as $x)
-    {
-        echo $x;
-    }
-if(isset($_COOKIE['recentlyVisited']))
-    echo "true";
-else
-    echo "false";
+setcookie('recentlyVisited', $result);
+$_COOKIE['recentlyVisited'] = $result;
 
 include 'FullSizeViewTemplate.php';
