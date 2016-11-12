ef4825cce792aa73a55e362ec8c6580fee3fa851
Umang Saxena <umang.saxena@sjsu.edu>
2016-11-12 03:04:16 -0800

    Changes

--- signin/usersFromcURL.php
+++ signin/usersFromcURL.php
@@ -56,40 +56,42 @@ $users2 = json_decode($users2, true);
     </style>
 </head>
 <body>
-<div class='row' style='font-weight: bold'>
-    </br>
-    <div style="text-align: center">Data from API : 'https://juranet.tech/php/curl/user_list.php'</div>
-    </br>
-    <hr>
-    <?php
+    <div class="container">
+        <div class='row' style='font-weight: bold'>
+        </br>
+        <div style="text-align: center">Data from API : 'https://juranet.tech/php/curl/user_list.php'</div>
+        </br>
+        <hr>
+        <?php
 
-    foreach ($users1 as $user)
-    {
+        foreach ($users1 as $user)
+        {
+            echo "
+        <div class='col-md-2'>First Name : " . $user['Firstname'] . "</div>
+        <div class='col-md-2'>Last Name : " . $user['LastName'] . "</div>
+        <div class='col-md-2'>Home Address : " . $user['HomeAddr'] . "</div>
+        <div class='col-md-2'>Home Phone : " . $user['Homephone'] . "</div>
+        <div class='col-md-2'>Cell Phone : " . $user['Cellphone'] . "</div>
+        <div class='col-md-2'>Email : " . $user['Email'] . "</div></br></br><hr>";
+        }
+
         echo "
-    <div class='col-md-2'>First Name : " . $user['Firstname'] . "</div>
-    <div class='col-md-2'>Last Name : " . $user['LastName'] . "</div>
-    <div class='col-md-2'>Home Address : " . $user['HomeAddr'] . "</div>
-    <div class='col-md-2'>Home Phone : " . $user['Homephone'] . "</div>
-    <div class='col-md-2'>Cell Phone : " . $user['Cellphone'] . "</div>
-    <div class='col-md-2'>Email : " . $user['Email'] . "</div></br></br><hr>";
-    }
+        </br>
+            <div style=\"text-align: center\">Data from API : 'http://geekbabu.com/api/users.php'</div>
+        </br>
+        <hr>";
+        foreach ($users2 as $user)
+        {
+            echo "
+        <div class='col-md-2'>User Name : " . $user['username'] . "</div>
+        <div class='col-md-2'>First Name : " . $user['firstname'] . "</div>
+        <div class='col-md-2'>Last Name : " . $user['lastName'] . "</div>
+        <div class='col-md-2'>Gender : " . $user['gender'] . "</div>
+        <div class='col-md-2'>Age : " . $user['age'] . "</div></br></br><hr>";
+        }
+        ?>
 
-    echo "
-    </br>
-        <div style=\"text-align: center\">Data from API : 'http://geekbabu.com/api/users.php'</div>
-    </br>
-    <hr>";
-    foreach ($users2 as $user)
-    {
-        echo "
-    <div class='col-md-2'>User Name : " . $user['username'] . "</div>
-    <div class='col-md-2'>First Name : " . $user['firstname'] . "</div>
-    <div class='col-md-2'>Last Name : " . $user['lastName'] . "</div>
-    <div class='col-md-2'>Gender : " . $user['gender'] . "</div>
-    <div class='col-md-2'>Age : " . $user['age'] . "</div></br></br><hr>";
-    }
-    ?>
-
-</div>
+    </div>
+    </div>
 </body>
 </html>
