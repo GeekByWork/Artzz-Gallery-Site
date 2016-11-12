606ecaf5bda93ee1fe29f353c8487963cadb5ea0
Umang Saxena <umang.saxena@sjsu.edu>
2016-11-12 02:33:21 -0800

    Changes

--- signin/usersFromcURL.php
+++ signin/usersFromcURL.php
@@ -9,9 +9,28 @@ curl_setopt($ch, CURLOPT_URL, "https://juranet.tech/php/curl/user_list.php");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 // $output contains the output string
-$output = curl_exec($ch);
+$users = curl_exec($ch);
 
 // close curl resource to free up system resources
 curl_close($ch);
 
-echo $output;
+?>
+
+<!DOCTYPE html>
+<html>
+<div class='row' style='font-weight: bold'>
+    <?php
+    foreach ($users as $user)
+    {
+        echo "
+    <div class='col-md-2'>" . $user['Firstname'] . "</div>
+    <div class='col-md-2'>" . $user['LastName'] . "</div>
+    <div class='col-md-2'>" . $user['HomeAddr'] . "</div>
+    <div class='col-md-2'>" . $user['Homephone'] . "</div>
+    <div class='col-md-2'>" . $user['Cellphone'] . "</div>
+    <div class='col-md-2'>" . $user['Email'] . "</div>";
+    }
+    ?>
+
+</div>
+</html>
