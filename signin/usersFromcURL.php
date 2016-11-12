d1be2cec68eda0c1f28e01ad253ba3e711c6a73b
Umang Saxena <umang.saxena@sjsu.edu>
2016-11-12 02:37:09 -0800

    Changes

--- signin/usersFromcURL.php
+++ signin/usersFromcURL.php
@@ -13,22 +13,3 @@ $users = json_decode(curl_exec($ch), true);
 curl_close($ch);
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<div class='row' style='font-weight: bold'>
-    <?php
-    foreach ($users as $user)
-    {
-        echo "
-    <div class='col-md-2'>" . $user['Firstname'] . "</div>
-    <div class='col-md-2'>" . $user['LastName'] . "</div>
-    <div class='col-md-2'>" . $user['HomeAddr'] . "</div>
-    <div class='col-md-2'>" . $user['Homephone'] . "</div>
-    <div class='col-md-2'>" . $user['Cellphone'] . "</div>
-    <div class='col-md-2'>" . $user['Email'] . "</div>";
-    }
-    ?>
-
-</div>
-</html>
