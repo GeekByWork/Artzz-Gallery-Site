0814f57f731973cbf88d3ae80a81f009ef8cc304
Umang Saxena <umang.saxena@sjsu.edu>
2016-11-12 01:13:47 -0800

    Changes

--- signin/usersFromcURL.php
+++ signin/usersFromcURL.php
@@ -1,14 +1,9 @@
 <?php
-// Get cURL resource
-$curl = curl_init();
-// Set some options - we are passing in a useragent too here
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'http://google.com',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
-// Send the request & save response to $resp
-$resp = curl_exec($curl);
-echo $resp;
-// Close request to clear up some resources
-curl_close($curl);
+$ch = curl_init();
+$timeout = 5;
+curl_setopt($ch, CURLOPT_URL, 'http://google.com');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+$data = curl_exec($ch);
+curl_close($ch);
+echo $data;
