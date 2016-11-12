0c5a3459ab752878c4c42924cf9124b8e1cbdb8f
Umang Saxena <umang.saxena@sjsu.edu>
2016-11-12 01:27:02 -0800

    Changes

--- signin/usersFromcURL.php
+++ signin/usersFromcURL.php
@@ -10,7 +10,8 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 // $output contains the output string
 $output = curl_exec($ch);
-echo $output;
 
 // close curl resource to free up system resources
 curl_close($ch);
+
+echo $output;
