dae4424d6dc2097f714f8adbafbd09863fd768af
Umang Saxena <umang.saxena@sjsu.edu>
2016-11-12 01:16:36 -0800

    Changes

--- signin/usersFromcURL.php
+++ signin/usersFromcURL.php
@@ -1,9 +1,9 @@
 <?php
 $ch = curl_init();
 $timeout = 5;
-curl_setopt($ch, CURLOPT_URL, 'http://google.com');
+curl_setopt($ch, CURLOPT_URL, 'https://juranet.tech/php/curl/user_list.php');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 $data = curl_exec($ch);
 curl_close($ch);
-echo $data;
+echo json_decode($data);
