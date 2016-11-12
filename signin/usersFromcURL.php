64f1bcfbd1cf92c77f32a7f81f2a785776083520
Umang Saxena <umang.saxena@sjsu.edu>
2016-11-12 02:58:25 -0800

    Changes

--- signin/usersFromcURL.php
+++ signin/usersFromcURL.php
@@ -29,9 +29,25 @@ $users2 = json_decode($users2, true);
 
 <!DOCTYPE html>
 <html>
+<head>
+    <link href="../assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles -->
+    <link href="../assets/css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/icomoon.css">
+    <link href="../assets/css/animate-custom.css" rel="stylesheet">
+
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+
+    <script src="../assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="../assets/js/modernizr.custom.js"></script>
+</head>
+<body>
 <div class='row' style='font-weight: bold'>
     </br>
-        <div style="text-align: center">Data from API : 'https://juranet.tech/php/curl/user_list.php'</div>
+    <div style="text-align: center">Data from API : 'https://juranet.tech/php/curl/user_list.php'</div>
     </br>
     <hr>
     <?php
@@ -64,4 +80,5 @@ $users2 = json_decode($users2, true);
     ?>
 
 </div>
+</body>
 </html>
