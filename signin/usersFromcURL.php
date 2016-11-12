75d26f9be8ab1e63a837800bb1398c9845eef5de
Umang Saxena <umang.saxena@sjsu.edu>
2016-11-12 02:47:46 -0800

    Changes

--- signin/usersFromcURL.php
+++ signin/usersFromcURL.php
@@ -9,12 +9,22 @@ curl_setopt($ch, CURLOPT_URL, "https://juranet.tech/php/curl/user_list.php");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 // $output contains the output string
-$users = curl_exec($ch);
+$users1 = curl_exec($ch);
+
+
+curl_setopt($ch, CURLOPT_URL, "http://geekbabu.com/api/users.php");
+
+//return the transfer as a string
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// $output contains the output string
+$users2 = curl_exec($ch);
 
 // close curl resource to free up system resources
 curl_close($ch);
 
-$users = json_decode($users, true);
+$users1 = json_decode($users1, true);
+$users2 = json_decode($users2, true);
 ?>
 
 <!DOCTYPE html>
@@ -22,7 +32,7 @@ $users = json_decode($users, true);
 <div class='row' style='font-weight: bold'>
     <?php
 
-    foreach ($users as $user)
+    foreach ($users1 as $user)
     {
         echo "
     <div class='col-md-2'>" . $user['Firstname'] . "</div>
@@ -31,6 +41,16 @@ $users = json_decode($users, true);
     <div class='col-md-2'>" . $user['Homephone'] . "</div>
     <div class='col-md-2'>" . $user['Cellphone'] . "</div>
     <div class='col-md-2'>" . $user['Email'] . "</div>";
+    }
+
+    foreach ($users2 as $user)
+    {
+        echo "
+    <div class='col-md-2'>" . $user['username'] . "</div>
+    <div class='col-md-2'>" . $user['firstname'] . "</div>
+    <div class='col-md-2'>" . $user['lastName'] . "</div>
+    <div class='col-md-2'>" . $user['gender'] . "</div>
+    <div class='col-md-2'>" . $user['age'] . "</div>";
     }
     ?>
 
