f22a62afe076d90457a3ab17dbaaeec518da1ce1
Umang Saxena <umang.saxena@sjsu.edu>
2016-11-30 01:41:49 -0800

    Forward Slash Change

--- api/products.php
+++ api/products.php
@@ -35,5 +35,5 @@ function get_products()
         mysqli_free_result($result);
     }
     $conn->close();
-    return json_encode(array_values($products), JSON_PRETTY_PRINT);
+    return json_encode(array_values($products), JSON_PRETTY_PRINT, JSON_UNESCAPED_SLASHES);
 }
