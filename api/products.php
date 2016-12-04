bc252a2f6b32c6b34fab57069d35011b24f892c7
Umang Saxena <umang.saxena@sjsu.edu>
2016-12-04 15:10:46 -0800

    "Individual Item Fix"

--- api/products.php
+++ api/products.php
@@ -48,7 +48,7 @@ function get_products($id)
     }
     $conn->close();
     if(count($products) == 1){
-        print array_values($products[0]);
+        print array_values($products[1]);
         return json_encode(array_values($products[0]), JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
     }
     else
